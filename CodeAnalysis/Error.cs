c5a924e6183226551b8a3feaac109fe7978f364a
Paul Brätz <paul.braetz@rhomicro.com>
2022-10-18 19:13:57 +0200

    Added Error type

---
+++ CodeAnalysis/Error.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace RhoMicro.CodeAnalysis
+{
+	internal readonly struct Error
+	{
+		private Error(ImmutableArray<Exception> exceptions)
+		{
+			Exceptions = exceptions;
+
+			_string =
+$@"/*
+An error has occured:
+{String.Join("\n\n", exceptions.Select(e => e.ToString()))}
+*/";
+		}
+
+		public readonly ImmutableArray<Exception> Exceptions;
+		private readonly string _string;
+
+		public static Error Create()
+		{
+			return new Error(ImmutableArray.Create<Exception>());
+		}
+		public Error With(Exception exception)
+		{
+			return new Error(Exceptions.Add(exception));
+		}
+		public Error With(params Exception[] exceptions)
+		{
+			return new Error(Exceptions.AddRange(exceptions));
+		}
+
+		public override String ToString()
+		{
+			return _string;
+		}
+
+		public static implicit operator String(Error error)
+		{
+			return error.ToString();
+		}
+	}
+}
--- CodeAnalysis/TypeIdentifier.cs
+++ CodeAnalysis/TypeIdentifier.cs
@@ -15,6 +15,8 @@ namespace RhoMicro.CodeAnalysis
 			Namespace = @namespace;
 		}
 
+		public static TypeIdentifier TemplateAttribute { get; internal set; }
+
 		public static TypeIdentifier Create<T>()
 		{
 			return Create(typeof(T));
